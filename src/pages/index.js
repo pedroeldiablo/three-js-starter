78b42e9471b265008030425cbc8cfbd5d6536ce9
Peter Williams <pedroeldiablo@yahoo.com>
2020-04-22 17:50:06 +0100

    add plane and shadows

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, {useState, useRef } from 'react';
+import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { Canvas, extend, useThree, useFrame } from 'react-three-fiber';
 import { useSpring, a } from 'react-spring/three';
@@ -26,29 +27,39 @@ const Controls = () => {
   );
 };
 
+const Plane = () => (
+  <mesh rotation={[-Math.PI/2, 0, 0]} position={[0, -0.5, 0]} receiveShadow>
+    <planeBufferGeometry attach="geometry" args={[100, 100]} />
+    <meshPhysicalMaterial attach="material" color="white" />
+  </mesh>
+);
+
 const Box = () => {
-  const mesh = useRef();
+//   const mesh = useRef();
   const [hovered, setHovered] = useState(false);
   const [active, setActive] = useState(false);
   const props = useSpring({
     scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
-    color: hovered ? 'pink' : 'blue'
+    color: hovered ? 'red' : 'blue'
 
   });
 
   return(
     <a.mesh 
-      ref={mesh}
+    //   ref={mesh}
       onPointerOver={() => setHovered(true)}
       onPointerOut={() => setHovered(false)}
       onClick={() => setActive(!active)}
-      scale={props.scale}
+	  scale={props.scale}
+	  castShadow
     >
+      <ambientLight intensity={0.8}/>
+      <spotLight position={[15, 20, 5]} penumbra={[1]} castShadow/>
       <boxBufferGeometry 
         attach="geometry"
         args={[1,1,1]}
       />
-      <a.meshBasicMaterial attach="material" color={props.color} />
+      <a.meshPhysicalMaterial attach="material" color={props.color} />
 
     </a.mesh>
   );
@@ -57,7 +68,15 @@ const Box = () => {
 
 
 export default () => 
-  <Canvas>
+  <Canvas 
+    camera={{ position: [0, 0, 5]}} 
+    onCreated={({gl}) => {
+      gl.shadowMap.enabled = true;
+      gl.shadowMap.type = THREE.PCFSoftShadowMap;
+    }}
+  >
+    <fog attach="fog" args={['black', 10, 25]} />
     <Controls />
+    <Plane />
     <Box />
   </Canvas>;
