99af9124955019c695ab2b146f3569cd98e095a6
Peter Williams <pedroeldiablo@yahoo.com>
2020-04-22 02:06:19 +0100

    replace previous with useSpring hook

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,33 @@
 import React, {useState} from 'react';
 import { Canvas } from 'react-three-fiber';
+import { useSpring, a } from 'react-spring/three';
 
 import './style.css';
 
 const Box = () => {
   const [hovered, setHovered] = useState(false);
   const [active, setActive] = useState(false);
+  const props = useSpring({
+    scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+    color: hovered ? 'pink' : 'blue'
+
+  });
+
+  console.log('rendered!');
+  
   return(
-    <mesh onPointerOver={() => setHovered(true)}
+    <a.mesh onPointerOver={() => setHovered(true)}
       onPointerOut={() => setHovered(false)}
       onClick={() => setActive(!active)}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      scale={props.scale}
     >
       <boxBufferGeometry 
         attach="geometry"
         args={[1,1,1]}
       />
-      <meshBasicMaterial attach="material" color={hovered ? 'pink' : 'blue'} />
+      <a.meshBasicMaterial attach="material" color={props.color} />
 
-    </mesh>
+    </a.mesh>
   );
 };
 
